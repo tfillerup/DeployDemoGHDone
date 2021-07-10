c080e71a26a8cea3e3a36413632ba253899111e6
Troy Fillerup <troy.fillerup@stgconsulting.com>
2021-07-09 21:00:24 -0600

    Added dbcontext

---
+++ DemoFunction/DemoFunction/Migrations/20210710030009_InitialMigration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DemoFunction.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Trail",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TrailType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Difficulty = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trail", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Trail");
+        }
+    }
+}
---
+++ DemoFunction/DemoFunction/Models/Trail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoFunction.Models
+{
+    public class Trail
+    {
+        public int Id { get; set; }
+        public string TrailType { get; set; }
+        public int Difficulty { get; set; }
+    }
+}
